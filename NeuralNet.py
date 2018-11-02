1a7fa057e625f903de2f22425450b9f5c8db943d
marzenaozimek <marzenaozimek@e-science.pl>
2018-11-02 16:44:32 +0100

    First model. no validation, no data augmentation.

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,16 +1,17 @@
 import os
 import nibabel as nib
 import numpy as np
+import math
 
-from keras import layers
-from keras import models
 from keras import optimizers
 from keras import backend as K
+from keras import Input
 from unet3d.model import unet
 
 train_data_path = 'C:\\Users\\Marzena\\Documents\\MOJE\\STUDIA\\PRACA_INZ\\data\\train_data'
 inputs_path = os.path.join(train_data_path, 'inputs')
 labels_path = os.path.join(train_data_path, 'labels')
+
 
 # generator, ktory bedzie podawal kolejne próbki (moje inputy do sieci)
 # nie potrzebuje przechowywac wszystkich na raz w tablicy numpy (i nie mam tyle miejsca)
@@ -28,12 +29,12 @@ def find_max(imgs_path):
     return max_value
 
 
-def data_generator(inputs_path, labels_path, start, stop):
+def data_generator(inputs_path, labels_path, start, stop, how_many=1):
     max_value1 = find_max(inputs_path)
     max_value2 = find_max(labels_path)
 
-    input_images = np.zeros(shape=(8, 4, 240, 240, 155))  # 8 - wielkość paczki danych
-    masks = np.zeros(shape=(8, 240, 240, 155))
+    input_images = np.zeros(shape=(how_many, 4, 240, 240, 155))  # how_many - wielkość paczki danych
+    masks = np.zeros(shape=(how_many, 240, 240, 155))
     for brain_nr in range(start, stop):
         for seq_nr in range(4):
             img_path = os.path.join(inputs_path, os.listdir(inputs_path)[brain_nr * 4 + seq_nr])
@@ -41,17 +42,17 @@ def data_generator(inputs_path, labels_path, start, stop):
             # przeskalowanie wartosci tak, by byly w zakresie (0,1)
             img_data = img_data.astype(float)
             img_data /= max_value1
-            input_images[brain_nr % 8][seq_nr] = img_data
+            input_images[brain_nr % how_many][seq_nr] = img_data
         seg_path = os.path.join(labels_path, os.listdir(labels_path)[brain_nr])
         seg_data = nib.load(seg_path).get_data()
         seg_data = seg_data.astype(float)
         seg_data /= max_value2
         masks[brain_nr] = seg_data
 
-        if (brain_nr % 8 == 7) or (brain_nr == stop - 1):  # co 8 iterację generuje tablicę
+        if (brain_nr % how_many == how_many - 1) or (brain_nr == stop - 1):  # co 'how_many' iterację generuje tablicę
             yield (input_images, masks)
-        # generator podaje tablicę o wielkości (8, 4, 240, 240, 155)
-        # (8 mózgów w 4 sekwencjach)
+        # generator podaje tablicę o wielkości (how_many, 4, 240, 240, 155)
+        # (how_many mózgów w 4 sekwencjach)
 
 
 # źródło: https://github.com/jocicmarko/ultrasound-nerve-segmentation/blob/master/train.py
@@ -61,10 +62,39 @@ def dice_coef(y_true, y_pred):
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + 1.) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1.)
 
+
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
 
+
 # koniec kodu ze źródła
+
+input_img = Input(shape=(4, 240, 240, 155), name='img')
+model = unet.get_unet(input_img, n_filters=16)
+
+model.compile(optimizer=optimizers.Adam(lr=1e-5),
+              loss="binary_crossentropy",
+              metrics=[dice_coef])
+model.summary()
+
+brains_number = len(os.listdir(labels_path)) - 1  # minus 1 bo ostatni plik to desktop.ini
+# brains_number = 285
+# dane uczące: 80% próbek, dane testowe: reszta
+train_set_size = math.floor(0.8 * brains_number)
+train_generator = data_generator(inputs_path, labels_path, 0, train_set_size)
+
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=train_set_size / 1,
+                              epochs=100)
+
+model.save('u_net_1.h5')
+print(history.history.keys())
+
+test_generator = data_generator(inputs_path, labels_path, train_set_size, brains_number)
+results = model.evaluate_generator(test_generator)
+print(results)
+
+# DO WYWALENIA
 
 # model = models.Sequential()
 # architektura sieci (warstwy)
@@ -83,27 +113,3 @@ def dice_coef_loss(y_true, y_pred):
 # model.add(layers.Dense(512, activation='relu'))
 # model.add(layers.Dense(8928000, activation='softmax'))  # na wyjsciu: dla kazdego piksela
 # # prawdopodobienstwo przynaleznosci do danej klasy (guz/ zdrowa tkanka)
-
-
-model = unet.get_unet(...)
-
-model.compile(optimizer=optimizers.Adam(lr=1e-5),
-              loss=dice_coef_loss, # może binary_crossentropy ??
-              metrics=[dice_coef])
-
-
-brains_number = len(os.listdir(labels_path)) - 1  # minus 1 bo ostatni plik to desktop.ini
-# brains_number = 285
-# dane uczące: 200 próbek, dane testowe: 85 próbek
-train_generator = data_generator(inputs_path, labels_path, 0, 200)
-test_generator = data_generator(inputs_path, labels_path, 200, brains_number)
-
-history = model.fit_generator(train_generator,
-                              steps_per_epoch=25,
-                              epochs=100)
-
-model.save('u_net_1.h5')
-print(history.history.keys())
-
-results = model.evaluate_generator(test_generator)
-print(results)
--- unet3d/model/unet.py
+++ unet3d/model/unet.py
@@ -6,10 +6,11 @@ from keras.layers.merge import concatenate
 
 
 def conv3d_block(input_tensor, n_filters, kernel_size, batchnorm=True):
-
     # first layer
     x = Conv3D(filters=n_filters, kernel_size=kernel_size,
                activation="relu", kernel_initializer="he_normal")(input_tensor)
+    # he_normal --> inicjalizacja wag wartosciami z rozkladu Gaussa z odchyleniem
+    # standardowym sqrt(2/N) (wyjasnienie w zeszycie! (str.12)
 
     if batchnorm:
         x = BatchNormalization()(x)
@@ -23,46 +24,58 @@ def conv3d_block(input_tensor, n_filters, kernel_size, batchnorm=True):
 
     return x
 
-def get_unet(input_img, n_filters, batchnorm=True):
 
+def get_unet(input_img, n_filters, batchnorm=True):
     # input_img --> wielkość wejściowego obrazu
+    # domyslnie data_format to channels_last
 
     # contracting path
-    c1 = conv3d_block(input_img, n_filters=n_filters*1, kernel_size=3, batchnorm=batchnorm)
+    c1 = conv3d_block(input_img, n_filters=n_filters * 1, kernel_size=3,
+                      data_format="channels_first", batchnorm=batchnorm)
     p1 = MaxPooling3D(pool_size=(2, 2, 2))(c1)
 
-    c2 = conv3d_block(p1, n_filters=n_filters*2, kernel_size=3, batchnorm=batchnorm)
+    c2 = conv3d_block(p1, n_filters=n_filters * 2, kernel_size=3,
+                      data_format="channels_first", batchnorm=batchnorm)
     p2 = MaxPooling3D((2, 2, 2))(c2)
 
-    c3 = conv3d_block(p2, n_filters=n_filters*4, kernel_size=3, batchnorm=batchnorm)
+    c3 = conv3d_block(p2, n_filters=n_filters * 4, kernel_size=3,
+                      data_format="channels_first", batchnorm=batchnorm)
     p3 = MaxPooling3D((2, 2, 2))(c3)
 
-    c4 = conv3d_block(p3, n_filters=n_filters*8, kernel_size=3, batchnorm=batchnorm)
+    c4 = conv3d_block(p3, n_filters=n_filters * 8, kernel_size=3,
+                      data_format="channels_first", batchnorm=batchnorm)
     p4 = MaxPooling3D((2, 2, 2))(c4)
 
-    c5 = conv3d_block(p4, n_filters=n_filters*16, kernel_size=3, batchnorm=batchnorm)
+    c5 = conv3d_block(p4, n_filters=n_filters * 16, kernel_size=3,
+                      data_format="channels_first", batchnorm=batchnorm)
 
     # expansive path
-    u6 = Conv3DTranspose(n_filters*8, kernel_size=3, strides=2, batchnorm=batchnorm)(c5)
+    u6 = Conv3DTranspose(n_filters * 8, kernel_size=3, strides=2,
+                         data_format="channels_first", batchnorm=batchnorm)(c5)
     u6 = concatenate([u6, c4])
-    c6 = conv3d_block(u6, n_filters*8, kernel_size=3, batchnorm=batchnorm)
+    c6 = conv3d_block(u6, n_filters * 8, kernel_size=3,
+                      data_format="channels_first", batchnorm=batchnorm)
 
-    u7 = Conv3DTranspose(n_filters*4, kernel_size=3, strides=2, batchnorm=batchnorm)(c6)
+    u7 = Conv3DTranspose(n_filters * 4, kernel_size=3, strides=2,
+                         data_format="channels_first", batchnorm=batchnorm)(c6)
     u7 = concatenate([u7, c3])
-    c7 = conv3d_block(u7, n_filters*4, kernel_size=3, batchnorm=batchnorm)
+    c7 = conv3d_block(u7, n_filters * 4, kernel_size=3,
+                      data_format="channels_first", batchnorm=batchnorm)
 
-    u8 = Conv3DTranspose(n_filters*2, kernel_size=3, strides=2, batchnorm=batchnorm)(c7)
+    u8 = Conv3DTranspose(n_filters * 2, kernel_size=3, strides=2,
+                         data_format="channels_first", batchnorm=batchnorm)(c7)
     u8 = concatenate([u8, c2])
-    c8 = conv3d_block(u8, n_filters*2, kernel_size=3, batchnorm=batchnorm)
+    c8 = conv3d_block(u8, n_filters * 2, kernel_size=3,
+                      data_format="channels_first", batchnorm=batchnorm)
 
-    u9 = Conv3DTranspose(n_filters*1, kernel_size=3, strides=2, batchnorm=batchnorm)(c8)
+    u9 = Conv3DTranspose(n_filters * 1, kernel_size=3, strides=2,
+                         data_format="channels_first", batchnorm=batchnorm)(c8)
     u9 = concatenate([u9, c1])
-    c9 = conv3d_block(u9, n_filters*1, kernel_size=3, batchnorm=batchnorm)
+    c9 = conv3d_block(u9, n_filters * 1, kernel_size=3,
+                      data_format="channels_first", batchnorm=batchnorm)
 
-    # output mask image
-
-    outputs = Conv3D() # ??
-    model = Model(inputs=[input_img], outputs=outputs)
+    # output - wysegmentowany obraz
+    outputs = Conv3D(1, (1, 1), data_format="channels_first", activation='sigmoid')(c9)
+    model = Model(inputs=[input_img], outputs=[outputs])
 
     return model
-
